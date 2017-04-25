716100ef17cc546b328218d85bfeb1b29b9c19c7
Emerson Torres <emerson.torres.tec@gmail.com>
2017-04-25 16:38:48 -0300

    Adicionado JPA no projeto, integracao entre EJB e JPA, controle de Pool de conexoes e Datasource para MySQL no WildFly

--- src/br/com/caelum/livraria/dao/UsuarioDao.java
+++ src/br/com/caelum/livraria/dao/UsuarioDao.java
@@ -1,18 +1,22 @@
 package br.com.caelum.livraria.dao;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import br.com.caelum.livraria.modelo.Usuario;
 
 @Stateless
 public class UsuarioDao {
 
-	@Inject
-	private Banco banco;
+	@PersistenceContext
+	private EntityManager em;
 
 	public Usuario buscaPeloLogin(String login) {
-		return this.banco.buscaPeloNome(login);
+		TypedQuery<Usuario> query = em.createQuery("select u from Usuario u where u.login = :pLogin", Usuario.class);
+		query.setParameter("pLogin", login);
+		return query.getSingleResult();
 	}
 	
 }
